e29c33ee8dcd98c88902906a73d869cd22d0cba8
majid <zargostar@yahoo.com>
2024-01-30 08:20:23 +0330

    good by

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
+        <div>Good by</div>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
